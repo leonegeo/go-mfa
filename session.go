fb541dda4e3fac86760c415ae1a651dd45007710
mpg <mpg@flaxen.com>
2018-01-10 11:17:30 -0500

    add support for “noninteractive” sessions

--- session.go
+++ session.go
@@ -4,6 +4,7 @@
 package mfacache
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws/credentials"
@@ -13,6 +14,7 @@ import (
 
 // NewSession returns a new session, using the cache: you can use this
 // function as an example of how to do things in your own app.
+// This is an interactive session, i.e. it will ask on stdin for your token.
 func NewSession(duration time.Duration) (*session.Session, error) {
 
 	opts := session.Options{
@@ -32,5 +34,42 @@ func NewSession(duration time.Duration) (*session.Session, error) {
 	// Inject cache able credential provider on top of the SDK's credentials loader
 	sess.Config.Credentials = credentials.NewCredentials(provider)
 
+	_, err = sess.Config.Credentials.Get()
+	if err != nil {
+		return nil, err
+	}
+	return sess, nil
+}
+
+// NewNoninteractiveSession makes a session for hands-free usage: if a token
+// is required, it'll just error out. You can use the supplied app to generate
+// the token from the command line.
+func NewNoninteractiveSession() (*session.Session, error) {
+
+	f := func() (string, error) {
+		return "", fmt.Errorf("MFA token not cached")
+	}
+
+	opts := session.Options{
+		SharedConfigState:       session.SharedConfigEnable,
+		AssumeRoleTokenProvider: f,
+	}
+	sess, err := session.NewSessionWithOptions(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	provider := &FileCacheProvider{
+		Creds:    sess.Config.Credentials,
+		Duration: 0,
+	}
+
+	// Inject cache able credential provider on top of the SDK's credentials loader
+	sess.Config.Credentials = credentials.NewCredentials(provider)
+
+	_, err = sess.Config.Credentials.Get()
+	if err != nil {
+		return nil, err
+	}
 	return sess, nil
 }
